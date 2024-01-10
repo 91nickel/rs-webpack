3d5f16b5f239b7b7a9c007c5b03f1ca80aab5e91
91nickel <91nickel@gmail.com>
2024-01-10 16:43:13 +0300

    optimization

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,35 @@
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const {merge} = require('webpack-merge')
 const commonConfig = require('./webpack.config.common')
 
 module.exports = merge(commonConfig, {
     mode: 'production',
     devtool: 'source-map',
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false,
+            reportFilename: 'report.html',
+        }),
+    ],
     optimization: {
         minimize: true,
         minimizer: [
             new CssMinimizerPlugin(), // Оптимизация CSS
             new TerserPlugin(), // Оптимизация JS
         ],
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: false,
+                vendor: {
+                    chunks: 'all',
+                    name: 'vendor',
+                    test: /node_modules/,
+                }
+            }
+        },
     },
 })
